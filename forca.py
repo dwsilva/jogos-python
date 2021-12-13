cf5e7c8b49e254b132b5a57eeb8b5e1d7c8d0f39
David Couto <coutodwsc@gmail.com>
2021-12-13 08:44:20 -0300

    Inseridos: validador de acertos; verificador de erros; encerramento do loop de chute

--- forca.py
+++ forca.py
@@ -3,26 +3,37 @@ def jogar():
     print("****Bem vindo ao jogo de Forca!****")
     print("***********************************")
 
-    palavra_secreta = "banana"
-    letras_acertada = ["_", "_", "_", "_", "_", "_",]
+    palavra_secreta = "banana".upper()
+    letras_acertada = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertada)
 
     while (not enforcou and not acertou):
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertada[index] = letra
-            index += 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute.upper() == letra.upper()):
+                    letras_acertada[index] = letra
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertada
         print(letras_acertada)
 
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
     print("Fim de jogo!")
+
 
 
 # Caso o arquivo forca.py seja executado diretamente, ele executa a função jogar()
